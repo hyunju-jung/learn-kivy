a614daaa02b12020366e2f67edd7c975506ee300
Hyunju Jung <hyunju.jung313@gmail.com>
2021-03-04 21:35:54 +0100

    my first Kivy experience

--- intro/01_hello_kivy.py
+++ intro/01_hello_kivy.py
@@ -1,13 +1,16 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
+#Every Kivy application needs to subclass App
+#and override build()
 class MainApp(App):
 	def build(self):
-		label = Label(text="Hello from Kivy", size_hint=(.5, .5),
+		label = Label(text="Hello from Kivy",
+			size_hint=(.5, .5), #width, height
 			pos_hint={'center_x': .5, 'center_y': .5})
 
 		return label
 
 if __name__ == '__main__':
 	app=MainApp()
-	app.run()
+	app.run() #to make the application run
